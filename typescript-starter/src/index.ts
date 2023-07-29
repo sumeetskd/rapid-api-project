c3ea27263919429779096e13ea78d6b142352f93
Sumeet Kumar Das <41441163+sumeetskd@users.noreply.github.com>
2023-07-29 18:11:42 +0530

    changes to index.ts

--- typescript-starter/src/index.ts
+++ typescript-starter/src/index.ts
@@ -1,6 +1,6 @@
 import express, {Express, Request, Response} from "express";
 import dotenv from 'dotenv';
-import { getXataClient,Ingredients  } from "./xata";
+import { getXataClient,Ingredients,Orderpizza  } from "./xata";
 
 dotenv.config(); // make sure that env variables are included
 
@@ -18,7 +18,7 @@ type MyResponse<T> =
     data: any;
 }
 
-app.get('/api/jobs', async (req: Request, res: Response<MyResponse<Ingredients[]>>)=>{
+app.get('/ingredients/allitems', async (req: Request, res: Response<MyResponse<Ingredients[]>>)=>{
     try {
         const data = await xata.db.ingredients.getAll();
         return res.status(200).json({data:data});
@@ -28,7 +28,7 @@ app.get('/api/jobs', async (req: Request, res: Response<MyResponse<Ingredients[]
       }
 })
 
-app.post('/api/jobs', async (req: Request<{},{},Ingredients>, res: Response<MyResponse<Ingredients>>)=>{
+app.post('/ingredients/additem', async (req: Request<{},{},Ingredients>, res: Response<MyResponse<Ingredients>>)=>{
     try{
         const item = req.body;
         const createdJob = await xata.db.ingredients.create(item);
@@ -41,7 +41,7 @@ app.post('/api/jobs', async (req: Request<{},{},Ingredients>, res: Response<MyRe
     }      
 })
 
-app.put('/api/jobs/:id', async (req: Request<{id:string},{},Ingredients>, res: Response<MyResponse<Ingredients>>)=>{
+app.put('/ingredients/modifyitem/:id', async (req: Request<{id:string},{},Ingredients>, res: Response<MyResponse<Ingredients>>)=>{
     try{
         const id = req.params.id;   //params.id has to match the url parameter /:id
         const item = req.body;
@@ -60,13 +60,72 @@ app.put('/api/jobs/:id', async (req: Request<{id:string},{},Ingredients>, res: R
     
 })
 
-app.delete('/api/jobs/:id', 
+app.delete('/ingredients/deleteitem/:id', 
 async (req: Request<{id:string},{},{}>, res: Response<MyResponse<Ingredients>>)=>{
     try{
         const id = req.params.id;
         const deleteRecord = await xata.db.ingredients.delete(id);
         if(!deleteRecord){
             return res.status(404).json({err: "Ingredient Not Found"})
+        }
+        return res.status(200).json({data:deleteRecord});
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({err:'Something went wrong'});
+    }
+
+})
+
+app.get('/orderpizza/allitems', async (req: Request, res: Response<MyResponse<Orderpizza[]>>)=>{
+    try {
+        const data = await xata.db.orderpizza.getAll();
+        return res.status(200).json({data:data});
+      } catch (err) {
+        console.error(err);
+        return res.status(500).json({err: 'Something went wrong' });
+      }
+})
+
+app.post('/orderpizza/additem', async (req: Request<{},{},Orderpizza>, res: Response<MyResponse<Orderpizza>>)=>{
+    try{
+        const item = req.body;
+        const createdJob = await xata.db.orderpizza.create(item);
+        // throw new Error('error')
+        return res.status(201).json({data:createdJob});
+    }
+    catch(err){
+        console.error(err);
+        return res.status(500).json({err:'Something went wrong'});
+    }      
+})
+
+app.put('/orderpizza/modifyitem/:id', async (req: Request<{id:string},{},Orderpizza>, res: Response<MyResponse<Orderpizza>>)=>{
+    try{
+        const id = req.params.id;   //params.id has to match the url parameter /:id
+        const item = req.body;
+        const updatedIng = await xata.db.orderpizza.update(id, item);
+
+        if(!updatedIng){
+            return res.status(404).json({err: 'Order Not Found'});
+        }
+
+        return res.status(201).json({data:updatedIng});
+    }
+    catch(err){
+        console.error(err);
+        return res.status(500).json({err:'Something went wrong'});
+    }
+    
+})
+
+app.delete('/Orderpizza/deleteitem/:id', 
+async (req: Request<{id:string},{},{}>, res: Response<MyResponse<Orderpizza>>)=>{
+    try{
+        const id = req.params.id;
+        const deleteRecord = await xata.db.orderpizza.delete(id);
+        if(!deleteRecord){
+            return res.status(404).json({err: "Order Not Found"})
         }
         return res.status(200).json({data:deleteRecord});
     }
